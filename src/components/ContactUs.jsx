7e8329981c104a98a42f9481d4cc680d1b38f40a
SanzharKadyrkulov <kadyrkulov.980@gmail.com>
2025-03-31 16:45:29 +0600

    form state

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -33,14 +33,14 @@ const ContactUs = () => {
 	const {
 		register,
 		handleSubmit,
-		formState: { errors, isValid, isDirty }
+		formState: { errors, isValid, isDirty, isSubmitSuccessful, isSubmitting }
 	} = useForm({
 		mode: 'onChange',
 		defaultValues
 	})
 
 	const onSubmit = (data) => {
-		send_message(data)
+		return send_message(data)
 	}
 
 	return (
@@ -95,11 +95,48 @@ const ContactUs = () => {
 					</div>
 
 					<button
-						disabled={!isValid && isDirty}
+						disabled={(!isValid && isDirty) || isSubmitting || isSubmitSuccessful}
 						type="submit"
-						className="font-pixel relative col-span-2 cursor-pointer bg-[#BF00FF] p-3 text-center text-lg leading-[87%] tracking-[-1px] text-white shadow-[6.05px_6.05px_0px_0px_#6A17C2] hover:bg-[#921EB9] hover:shadow-[6.05px_6.05px_0px_0px_#400084] active:bg-[#D968FF] active:shadow-[6.05px_6.05px_0px_0px_#7633BD] disabled:cursor-not-allowed disabled:opacity-50"
+						className="font-pixel relative col-span-2 cursor-pointer bg-[#BF00FF] p-3 text-center text-lg leading-[87%] tracking-[-1px] text-white shadow-[6.05px_6.05px_0px_0px_#6A17C2] hover:bg-[#921EB9] hover:shadow-[6.05px_6.05px_0px_0px_#400084] active:bg-[#D968FF] active:shadow-[6.05px_6.05px_0px_0px_#7633BD] disabled:cursor-not-allowed disabled:opacity-50 flex items-center justify-center"
 					>
-						Send
+						{isSubmitting && (
+							<>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									strokeWidth="1.5"
+									stroke="currentColor"
+									className="size-6 animate-[spin_2s_linear_infinite]"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z"
+									/>
+								</svg>
+							</>
+						)}
+
+						{isSubmitSuccessful && (
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth="1.5"
+								stroke="currentColor"
+								className="size-6"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+								/>
+							</svg>
+						)}
+
+						{!isSubmitting && !isSubmitSuccessful && 'Send'}
+
 						<div className="absolute top-0 left-[26px] h-full w-[15px] -skew-x-[30deg] transform bg-white opacity-15"></div>
 						<div className="absolute top-0 left-[48px] h-full w-2.5 -skew-x-[30deg] transform bg-white opacity-15"></div>
 					</button>
