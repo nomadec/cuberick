2fc71ef0ba194d8da3ddf36a3abf5c2abc7e8386
SanzharKadyrkulov <kadyrkulov.980@gmail.com>
2025-04-24 09:19:14 +0600

    change join squad link

--- src/shared/consts.js
+++ src/shared/consts.js
@@ -1,2 +1,3 @@
 export const BASE_URL = import.meta.env.VITE_BASE_URL
-export const MINI_APP_URL = 'https://t.me/cuberickgame_bot/cuberick'
+export const MINI_APP_URL =
+	'https://t.me/cuberickgame_bot/cuberick?startapp=7417bb16-fe21-4813-97e8-f614317bc7be'
